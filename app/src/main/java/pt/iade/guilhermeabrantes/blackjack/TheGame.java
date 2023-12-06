83ce6f884737f99f89a0e55e77dda522fef5e758
Guilherme Quinteiro Louro de Carvalho Abrantes <guiabrantes10b@gmail.com>
2023-12-06 18:31:03 +0000

    Starting the new page "the game"

--- app/src/main/java/pt/iade/guilhermeabrantes/blackjack/TheGame.java
+++ app/src/main/java/pt/iade/guilhermeabrantes/blackjack/TheGame.java
@@ -2,13 +2,33 @@ package pt.iade.guilhermeabrantes.blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TheGame extends AppCompatActivity {
+
+    private Button btnLeaveTable;
+    private Button btnStay;
+    private Button btnHit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_game);
+
+        btnLeaveTable = (Button) findViewById(R.id.btnLeaveGame);
+        btnHit = (Button) findViewById(R.id.btnHit);
+        btnStay = (Button) findViewById(R.id.btnStay);
+
+        btnLeaveTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TheGame.this, FrontPage.class));
+            }
+
+
+        });
     }
 }
