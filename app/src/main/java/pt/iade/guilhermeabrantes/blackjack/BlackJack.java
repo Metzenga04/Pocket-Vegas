959ca6af6f01658f5b557896363fd5e97d55b293
Guilherme Quinteiro Louro de Carvalho Abrantes <guiabrantes10b@gmail.com>
2023-12-11 11:59:10 +0000

    more changes in the new page war game and put the black jack game going

--- app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
+++ app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
@@ -5,25 +5,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import pt.iade.guilhermeabrantes.blackjack.models.Card;
 import pt.iade.guilhermeabrantes.blackjack.models.Deck;
+
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BlackJack extends AppCompatActivity {
 
     private Deck deck;
     private List<Card> playerHand;
     private List<Card> dealerHand;
+    private ImageView dealercard,dealercard2,dealercard3,dealercard4,dealercard5,playercard,playercard2,playercard3,playercard4,playercard5;
     private Button btnExit;
+    private TextView scoreDealer,scorePlayer;
     private Button btnHit;
     private Button btnStay;
+    private int sDealer = 0, sPlayer = 0;
     private Button btnStart;
     private LinearLayout linearLayout;
+    Random r;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -34,10 +42,23 @@ public class BlackJack extends AppCompatActivity {
         deck = new Deck();
         playerHand = new ArrayList<>();  // Distribui 2 cartas para o jogador
         dealerHand = new ArrayList<>();  // Distribui 2 cartas para o dealer
+        dealercard = (ImageView) findViewById(R.id.dealerCard);
+        playercard = (ImageView) findViewById(R.id.playerCard);
+        dealercard2 = (ImageView) findViewById(R.id.dealerCard2);
+        playercard2 = (ImageView) findViewById(R.id.playerCard2);
+        dealercard3 = (ImageView) findViewById(R.id.dealerCard3);
+        playercard3 = (ImageView) findViewById(R.id.playerCard3);
+        dealercard4 = (ImageView) findViewById(R.id.dealerCard4);
+        playercard4 = (ImageView) findViewById(R.id.playerCard4);
+        dealercard5 = (ImageView) findViewById(R.id.dealerCard5);
+        playercard5 = (ImageView) findViewById(R.id.playerCard5);
         btnExit = (Button) findViewById(R.id.btnExit);
         btnStay = (Button) findViewById(R.id.btnStay);
         btnHit = (Button) findViewById(R.id.btnHit);
-        btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        scorePlayer = (TextView) findViewById(R.id.scoreViewPlayer);
+        scoreDealer = (TextView) findViewById(R.id.scoreViewDealer);
+        r = new Random();
 
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +66,28 @@ public class BlackJack extends AppCompatActivity {
                 startActivity(new Intent(BlackJack.this, FrontPage.class));
             }
         });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dealCards();
+            }
+        });
     }
+        private void dealCards() {
+            // Draw cards for each player
+            int dealerCard = deck.drawCard();
+            int playerCard = deck.drawCard();
+            int playerCard2  = deck.drawCard();
+            int dealerCard2 = deck.drawCard();
 
+            // Set card images based on drawn cards
+            int dealerImage = getResources().getIdentifier( "card" + dealerCard, "drawable", getPackageName());
+            dealercard.setImageResource(dealerImage);
+
+            int playerImage = getResources().getIdentifier("card" + playerCard, "drawable", getPackageName());
+            playercard.setImageResource(playerImage);
+
+            int playerImage2 = getResources().getIdentifier("card" + playerCard2, "drawable", getPackageName());
+            playercard2.setImageResource(playerImage2);
+        }
 }
