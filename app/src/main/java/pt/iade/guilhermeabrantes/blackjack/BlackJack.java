b5267edb3e264488e7b2611694c2b5969c04a876
Guilherme Quinteiro Louro de Carvalho Abrantes <guiabrantes10b@gmail.com>
2023-12-11 14:16:27 +0000

    code changes in BlackJack.java

--- app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
+++ app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
@@ -6,115 +6,125 @@ import android.os.Bundle;
 import pt.iade.guilhermeabrantes.blackjack.models.Card;
 import pt.iade.guilhermeabrantes.blackjack.models.Deck;
 
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class BlackJack extends AppCompatActivity {
-
-    private Deck deck;
-    private List<Card> playerHand;
-    private List<Card> dealerHand;
-    private ImageView dealercard, dealercard2, dealercard3, dealercard4, dealercard5, playercard, playercard2, playercard3, playercard4, playercard5;
-    private Button btnExit;
-    private TextView scoreDealer, scorePlayer;
-    private Button btnHit;
-    private Button btnStand;
-    private int sDealer = 0, sPlayer = 0;
-    private Button btnStart;
-    private LinearLayout linearLayout;
-    Random r;
-
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_black_jack);
 
-        deck = new Deck();
-        playerHand = new ArrayList<>();  // Distribui 2 cartas para o jogador
-        dealerHand = new ArrayList<>();  // Distribui 2 cartas para o dealer
-        dealercard = (ImageView) findViewById(R.id.dealerCard);
-        playercard = (ImageView) findViewById(R.id.playerCard);
-        dealercard2 = (ImageView) findViewById(R.id.dealerCard2);
-        playercard2 = (ImageView) findViewById(R.id.playerCard2);
-        dealercard3 = (ImageView) findViewById(R.id.dealerCard3);
-        playercard3 = (ImageView) findViewById(R.id.playerCard3);
-        dealercard4 = (ImageView) findViewById(R.id.dealerCard4);
-        playercard4 = (ImageView) findViewById(R.id.playerCard4);
-        dealercard5 = (ImageView) findViewById(R.id.dealerCard5);
-        playercard5 = (ImageView) findViewById(R.id.playerCard5);
-        btnExit = (Button) findViewById(R.id.btnExit);
-        btnStand = (Button) findViewById(R.id.btnStand);
-        btnHit = (Button) findViewById(R.id.btnHit);
-        btnStart = (Button) findViewById(R.id.btnStart);
-        scorePlayer = (TextView) findViewById(R.id.scoreViewPlayer);
-        scoreDealer = (TextView) findViewById(R.id.scoreViewDealer);
-        r = new Random();
+        List<Card> pHand = new ArrayList<Card>();
+        List<Card> dHand = new ArrayList<>();
+        Button deal =findViewById(R.id.btnStart);
+        Button stand =findViewById(R.id.btnStand);
+        Button hit =findViewById(R.id.btnHit);
+        Button ok =findViewById(R.id.btnOk);
 
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(BlackJack.this, FrontPage.class));
-            }
+        ImageView card1 =findViewById(R.id.playerCard);
+        ImageView card2 =findViewById(R.id.playerCard2);
+        ImageView card3 =findViewById(R.id.playerCard3);
+
+        ImageView dCard1 =findViewById(R.id.dealerCard);
+        ImageView dCard2 =findViewById(R.id.dealerCard2);
+        ImageView dCard3 =findViewById(R.id.dealerCard3);
+
+        //TextView dialog =findViewById(R.id.scoreViewPlayer);
+
+        deal.setVisibility(View.VISIBLE);
+        stand.setVisibility(View.GONE);
+        hit.setVisibility(View.GONE);
+        ok.setVisibility(View.GONE);
+        card1.setVisibility(View.GONE);
+        card2.setVisibility(View.GONE);
+        card3.setVisibility(View.GONE);
+        dCard1.setVisibility(View.GONE);
+        dCard2.setVisibility(View.GONE);
+        dCard3.setVisibility(View.GONE);
+        //dialog.setVisibility(View.GONE);
+
+        deal.setOnClickListener(v -> {
+            deal.setVisibility(View.GONE);
+            stand.setVisibility(View.VISIBLE);
+            hit.setVisibility(View.VISIBLE);
+
+            Card first = new Card();
+            Card second = new Card();
+
+            pHand.add(first);
+            pHand.add(second);
+
+            card1.setImageResource(getResources().getIdentifier(first.getName(),"drawable",getPackageName()));
+            card2.setImageResource(getResources().getIdentifier(second.getName(),"drawable",getPackageName()));
+
+            card1.setVisibility(View.VISIBLE);
+            card2.setVisibility(View.VISIBLE);
+
+            Card dFirst = new Card();
+            Card dSecond =new Card();
+
+            dHand.add(dFirst);
+            dHand.add(dSecond);
+
+            dCard1.setImageResource(getResources().getIdentifier(dFirst.getName(),"drawable",getPackageName()));
+            dCard2.setImageResource(getResources().getIdentifier("cardback","drawable",getPackageName()));
+
+            dCard1.setVisibility(View.VISIBLE);
+            dCard2.setVisibility(View.VISIBLE);
         });
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View start) {
-                dealCards();
+        stand.setOnClickListener(v -> {
+            stand.setVisibility(View.GONE);
+            hit.setVisibility(View.GONE);
+
+            dCard2.setImageResource(getResources().getIdentifier(dHand.get(1).getName(),"drawable",getPackageName()));
+
+            boolean canHit =true;
+
+            while (canHit){
+                Card toAdd = new Card();
+                dHand.add(toAdd);
+
+                dCard3.setImageResource(getResources().getIdentifier(dHand.get(dHand.size() - 1).getName(),"drawable",getPackageName()));
+                dCard2.setImageResource(getResources().getIdentifier(dHand.get(dHand.size() - 2).getName(), "drawable",getPackageName()));
+                dCard1.setImageResource(getResources().getIdentifier(dHand.get(dHand.size() - 3).getName(),"drawable",getPackageName()));
+
+                dCard3.setVisibility(View.VISIBLE);
+                int sum =0;
+                for (int i = 0;i< dHand.size();i++){
+                    sum+=dHand.get(i).getRank();
+                    if (dHand.get(i).getRank()==11){
+                    }
+                }
+                if (sum > 16){
+                    canHit =false;
+                }
             }
-        });
-        btnHit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View hit) {
-                dealCards2();
+            int pSum = 0;
+            int pAces =0;
+            for (int i = 0;i < pHand.size();i++){
+                pSum += pHand.get(i).getRank();
+                if (pHand.get(i).getRank()==11){
+                    pAces++;
+                }
             }
-        });
-        btnStand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View stand) {
-                dealCards3();
+            if (pSum > 21 && pAces > 0){
+                pSum -= pAces * 10;
             }
+
         });
-    }
 
-    private void dealCards() {
-        // Draw cards for each player
-        int dealerCard = deck.drawCard();
-        int playerCard = deck.drawCard();
-        int playerCard2 = deck.drawCard();
 
-        // Set card images based on drawn cards
-        int dealerImage = getResources().getIdentifier("card" + dealerCard, "drawable", getPackageName());
-        dealercard.setImageResource(dealerImage);
-
-        int playerImage = getResources().getIdentifier("card" + playerCard, "drawable", getPackageName());
-        playercard.setImageResource(playerImage);
-
-        int playerImage2 = getResources().getIdentifier("card" + playerCard2, "drawable", getPackageName());
-        playercard2.setImageResource(playerImage2);
-    }
-
-    private void dealCards2() {
-        // Draw cards for each player
-        int playerCard3 = deck.drawCard();
-
-        int playerImage3 = getResources().getIdentifier("card" + playerCard3, "drawable", getPackageName());
-        playercard3.setImageResource(playerImage3);
-    }
-
-    private void dealCards3() {
-
-        int dealerCard2 = deck.drawCard();
-
-        // Set card images based on drawn cards
-        int dealerImage2 = getResources().getIdentifier("card" + dealerCard2, "drawable", getPackageName());
-        dealercard2.setImageResource(dealerImage2);
     }
 }
--- app/src/main/java/pt/iade/guilhermeabrantes/blackjack/models/Card.java
+++ app/src/main/java/pt/iade/guilhermeabrantes/blackjack/models/Card.java
@@ -1,37 +1,113 @@
 package pt.iade.guilhermeabrantes.blackjack.models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 public class Card {
-    private String suit;
-    private String rank;
+    public final static int ACE = 1;
+    public final static int Two = 2;
+    public final static int THREE = 3;
+    public final static int FOUR = 4;
+    public final static int FIVE = 5;
+    public final static int SIX = 6;
+    public final static int SEVEN = 7;
+    public final static int EIGHT = 8;
+    public final static int NINE = 9;
+    public final static int TEN = 10;
+    public final static int JACK = 11;
+    public final static int QUEEN = 12;
+    public final static int KING = 13;
 
-    public Card(String suit) {
-        this.suit = suit;
-        this.rank = rank;
-    }
+    //SUITS
+    public final static int DIAMONDS = 1;
+    public final static int CLUBS = 2;
+    public final static int HEARTS = 3;
+    public final static int SPADES = 4;
+    private int rank;
 
-    public String getSuit() {
-        return suit;
-    }
+    private int suit;
+    private String nameOfCard;
 
-    public String getRank() {
-        return rank;
-    }
+    public Card() {
+        int tempRank = (int) Math.floor(Math.random() * 13) + 1;
+        this.suit = (int) Math.floor(Math.random() * 4) + 1;
 
-    public int getCardValue() {
-        try {
-            int value = Integer.parseInt(rank);
-            return (value > 10) ? 10 : value;
-        } catch (NumberFormatException e) {
-            if ("A".equals(rank)) {
-                return 11; // Valor padrão para o Ás
-            } else {
-                return 10; // Valor padrão para cartas de figura
-            }
+        switch (tempRank) {
+            case ACE:
+                this.nameOfCard = "aceof";
+                this.rank = 11;
+                break;
+            case Two:
+                this.nameOfCard = "twoof";
+                this.rank = 2;
+                break;
+            case THREE:
+                this.nameOfCard = "threeof";
+                this.rank = 3;
+                break;
+            case FOUR:
+                this.nameOfCard = "fourof";
+                this.rank = 4;
+                break;
+            case FIVE:
+                this.nameOfCard = "fiveof";
+                this.rank = 5;
+                break;
+            case SIX:
+                this.nameOfCard = "sixof";
+                this.rank = 6;
+                break;
+            case SEVEN:
+                this.nameOfCard = "sevenof";
+                this.rank = 7;
+                break;
+            case EIGHT:
+                this.nameOfCard = "eightof";
+                this.rank = 8;
+                break;
+            case NINE:
+                this.nameOfCard = "nineof";
+                this.rank = 9;
+                break;
+            case TEN:
+                this.nameOfCard = "tenof";
+                this.rank = 10;
+                break;
+            case JACK:
+                this.nameOfCard = "jackof";
+                this.rank = 10;
+                break;
+            case QUEEN:
+                this.nameOfCard = "queenof";
+                this.rank = 10;
+                break;
+            case KING:
+                this.nameOfCard = "kingof";
+                this.rank = 10;
+                break;
+
+        }
+        switch (this.suit) {
+            case DIAMONDS:
+                this.nameOfCard = this.nameOfCard.concat("diamonds");
+                break;
+            case CLUBS:
+                this.nameOfCard = this.nameOfCard.concat("clubs");
+                break;
+            case HEARTS:
+                this.nameOfCard = this.nameOfCard.concat("hearts");
+                break;
+            case SPADES:
+                this.nameOfCard = this.nameOfCard.concat("spades");
+                break;
         }
     }
-
-    @Override
-    public String toString() {
-        return rank + " of " + suit;
+    public int getRank(){
+        return rank;
+    }
+    public String getName(){
+        return nameOfCard;
     }
 }
+
