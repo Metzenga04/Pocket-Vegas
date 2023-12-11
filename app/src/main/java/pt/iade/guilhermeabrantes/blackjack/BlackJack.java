04aa134b28d12902352497356f1d50a05a10723b
Guilherme Quinteiro Louro de Carvalho Abrantes <guiabrantes10b@gmail.com>
2023-12-11 12:18:31 +0000

    changes in black jack again

--- app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
+++ app/src/main/java/pt/iade/guilhermeabrantes/blackjack/BlackJack.java
@@ -6,13 +6,11 @@ import android.os.Bundle;
 import pt.iade.guilhermeabrantes.blackjack.models.Card;
 import pt.iade.guilhermeabrantes.blackjack.models.Deck;
 
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,11 +21,11 @@ public class BlackJack extends AppCompatActivity {
     private Deck deck;
     private List<Card> playerHand;
     private List<Card> dealerHand;
-    private ImageView dealercard,dealercard2,dealercard3,dealercard4,dealercard5,playercard,playercard2,playercard3,playercard4,playercard5;
+    private ImageView dealercard, dealercard2, dealercard3, dealercard4, dealercard5, playercard, playercard2, playercard3, playercard4, playercard5;
     private Button btnExit;
-    private TextView scoreDealer,scorePlayer;
+    private TextView scoreDealer, scorePlayer;
     private Button btnHit;
-    private Button btnStay;
+    private Button btnStand;
     private int sDealer = 0, sPlayer = 0;
     private Button btnStart;
     private LinearLayout linearLayout;
@@ -53,7 +51,7 @@ public class BlackJack extends AppCompatActivity {
         dealercard5 = (ImageView) findViewById(R.id.dealerCard5);
         playercard5 = (ImageView) findViewById(R.id.playerCard5);
         btnExit = (Button) findViewById(R.id.btnExit);
-        btnStay = (Button) findViewById(R.id.btnStay);
+        btnStand = (Button) findViewById(R.id.btnStand);
         btnHit = (Button) findViewById(R.id.btnHit);
         btnStart = (Button) findViewById(R.id.btnStart);
         scorePlayer = (TextView) findViewById(R.id.scoreViewPlayer);
@@ -68,26 +66,55 @@ public class BlackJack extends AppCompatActivity {
         });
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View start) {
                 dealCards();
             }
         });
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View hit) {
+                dealCards2();
+            }
+        });
+        btnStand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View stand) {
+                dealCards3();
+            }
+        });
     }
-        private void dealCards() {
-            // Draw cards for each player
-            int dealerCard = deck.drawCard();
-            int playerCard = deck.drawCard();
-            int playerCard2  = deck.drawCard();
-            int dealerCard2 = deck.drawCard();
 
-            // Set card images based on drawn cards
-            int dealerImage = getResources().getIdentifier( "card" + dealerCard, "drawable", getPackageName());
-            dealercard.setImageResource(dealerImage);
+    private void dealCards() {
+        // Draw cards for each player
+        int dealerCard = deck.drawCard();
+        int playerCard = deck.drawCard();
+        int playerCard2 = deck.drawCard();
 
-            int playerImage = getResources().getIdentifier("card" + playerCard, "drawable", getPackageName());
-            playercard.setImageResource(playerImage);
+        // Set card images based on drawn cards
+        int dealerImage = getResources().getIdentifier("card" + dealerCard, "drawable", getPackageName());
+        dealercard.setImageResource(dealerImage);
 
-            int playerImage2 = getResources().getIdentifier("card" + playerCard2, "drawable", getPackageName());
-            playercard2.setImageResource(playerImage2);
-        }
+        int playerImage = getResources().getIdentifier("card" + playerCard, "drawable", getPackageName());
+        playercard.setImageResource(playerImage);
+
+        int playerImage2 = getResources().getIdentifier("card" + playerCard2, "drawable", getPackageName());
+        playercard2.setImageResource(playerImage2);
+    }
+
+    private void dealCards2() {
+        // Draw cards for each player
+        int playerCard3 = deck.drawCard();
+
+        int playerImage3 = getResources().getIdentifier("card" + playerCard3, "drawable", getPackageName());
+        playercard3.setImageResource(playerImage3);
+    }
+
+    private void dealCards3() {
+
+        int dealerCard2 = deck.drawCard();
+
+        // Set card images based on drawn cards
+        int dealerImage2 = getResources().getIdentifier("card" + dealerCard2, "drawable", getPackageName());
+        dealercard2.setImageResource(dealerImage2);
+    }
 }
